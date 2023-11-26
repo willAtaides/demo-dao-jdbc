5839ce90cf00ec3cc96e1fab7b96649861164319
Willian Ataides <sg552.santos@gmail.com>
2023-11-26 13:49:23 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,9 @@ public class Program {
 			
 			
 			Seller seller = new Seller(21, "Willian", "willian.at@gmail.com", new Date(), 3000.0, obj);
+			
+			SellerDao sellerDao = DaoFactory.createSellerDao();
+			
 			System.out.println(obj);
 			System.out.println(seller);
 	}
